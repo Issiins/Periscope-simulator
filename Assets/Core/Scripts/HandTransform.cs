cac3aeafe38a4996d9973db061217ee87f1a7fc7
Issiins <160136271+Issiins@users.noreply.github.com>
2026-02-27 16:00:33 +0200

    Add hand transform script for easier acess

---
+++ Assets/Core/Scripts/HandTransform.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+namespace Core.Scripts
+{
+    [Serializable]
+    public class HandTransform
+    {
+        [SerializeField] private Transform leftHand; 
+        [SerializeField] private Transform rightHand;
+        public Transform LeftHand => leftHand;
+        public Transform RightHand => rightHand;
+    }
+}
--- Assets/Core/Scripts/RotationManager.cs
+++ Assets/Core/Scripts/RotationManager.cs
@@ -5,17 +5,14 @@ namespace Core.Scripts
     public class RotationManager : MonoBehaviour
     {
         [SerializeField, Tooltip("Object with IsGrabbed script")] private IsGrabbed isGrabbed;
-
-        [Header("Position of hands")] 
-        [SerializeField] private Transform leftHand; 
-        [SerializeField] private Transform rightHand;
+        [SerializeField] private HandTransform handPosition;
         private Quaternion _lookRotation;
         private Vector3 _direction; 
         void Update()
         {
             if (isGrabbed.BothGrabbed)
             {
-                Vector3 midPoint = TrackingPosition.CalculateMiddlePoints(leftHand, rightHand);
+                Vector3 midPoint = TrackingPosition.CalculateMiddlePoints(handPosition.LeftHand, handPosition.RightHand);
                 _direction = new Vector3(transform.position.x - midPoint.x, 
                     0f, 
                     transform.position.z - midPoint.z).normalized;
