909a33ca04b1eca188a77d595c1bfbc7ac09070e
Issiins <160136271+Issiins@users.noreply.github.com>
2026-03-02 14:44:32 +0200

    add handle grabbing reach limit

--- Assets/Core/Scripts/isGrabbed.cs
+++ Assets/Core/Scripts/isGrabbed.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
@@ -5,13 +6,25 @@ namespace Core.Scripts
 {
     public class IsGrabbed : MonoBehaviour
     {
-        [SerializeField] private XRGrabInteractable leftHandle; 
+        [SerializeField] private XRGrabInteractable leftHandle;
         [SerializeField] private XRGrabInteractable rightHandle;
+        [SerializeField] private HandTransform handPos;
+        [SerializeField] private float maxDistance = 1.5f;
         private bool _bothGrabbed;
         public bool BothGrabbed => _bothGrabbed;
         private void Update()
         {
-            _bothGrabbed = leftHandle.isSelected && rightHandle.isSelected;
+            bool leftGrabbed = leftHandle.isSelected;
+            bool rightGrabbed = rightHandle.isSelected;
+            _bothGrabbed = leftGrabbed && rightGrabbed;
+            if (Vector3.Distance(handPos.LeftHand.position, leftHandle.transform.position) > maxDistance)
+            {
+                leftHandle.interactionManager.CancelInteractableSelection((IXRSelectInteractable)leftHandle);
+            }
+            else if (Vector3.Distance(handPos.RightHand.position, leftHandle.transform.position) > maxDistance)
+            {
+                rightHandle.interactionManager.CancelInteractableSelection((IXRSelectInteractable)rightHandle);
+            }
         }
     }
 }
