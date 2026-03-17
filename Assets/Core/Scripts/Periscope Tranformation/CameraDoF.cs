2b29b9ab0c7a0d0f6bd649bb5098195ae4dad988
Issiins <160136271+Issiins@users.noreply.github.com>
2026-03-17 13:36:12 +0200

    Add control DoF via control table

---
+++ Assets/Core/Scripts/Periscope Tranformation/CameraDoF.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using Core.Scripts.Control_Table;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Core.Scripts.Periscope_Tranformation
+{
+    public class CameraDoF : MonoBehaviour
+    {
+        [SerializeField] private PositionChecker positionChecker;
+        [SerializeField] private float maxFocusLenght = 300f;
+         private float _coefficient;
+         private Volume _volume;
+         private DepthOfField _depthOfField;
+        
+         private void Start()
+         {
+             _volume = GetComponentInChildren<Volume>();
+             _volume.profile.TryGet(out _depthOfField);
+             _coefficient = positionChecker.DistanceCoeff;
+             StartCoroutine(CoeffientUpdate());
+         }
+
+         void Update()
+         {
+             _depthOfField.focalLength.value = maxFocusLenght*0.5f*(_coefficient + 1); 
+         }
+         private IEnumerator CoeffientUpdate()
+         {
+             while (true)
+             {
+                 _coefficient = positionChecker.DistanceCoeff;
+                 yield return new WaitForSeconds(0.5f);
+             }
+         }
+       
+    }
+}
