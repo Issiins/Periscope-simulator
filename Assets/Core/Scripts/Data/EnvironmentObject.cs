fe997307c66317721dffdc9fb6014108b44fa27c
Issiins <160136271+Issiins@users.noreply.github.com>
2026-04-28 15:49:09 +0300

    Create 'Find Object' minigame

---
+++ Assets/Core/Scripts/Data/EnvironmentObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Core.Scripts.Data
+{
+    [Serializable]
+    public class EnvironmentObject
+    {
+        [SerializeField] public Transform pos;
+        [SerializeField, CanBeNull] public string name;
+        [SerializeField, CanBeNull] public Sprite objectImage;
+    }
+}
---
+++ Assets/Core/Scripts/FIndObject.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using Core.Scripts.Data;
+using TMPro;
+using Unity.Collections;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Core.Scripts
+{
+    public class FIndObject : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private Camera periscopeCamera;
+        [SerializeField] private EnvironmentObject[] environmentObjects;
+        [SerializeField] private float threshHold = 0.98f;
+        [SerializeField] private float requiredTime = 3.0f;
+        
+        [Header("UI")]
+        [SerializeField] private Image objectImage;
+        [SerializeField] private TMP_Text objectText;
+        
+        [FormerlySerializedAs("_idx")]
+        [Header("Live Stats")]
+        [SerializeField, ReadOnly] private int idx = 0;
+        [SerializeField, ReadOnly] private float lookTimer = 0f;
+        [SerializeField, ReadOnly] private float currentDot;
+        private void Start()
+        {
+            if (environmentObjects.Length > 0) 
+            {
+                UpdateUI(environmentObjects[idx]);
+            }
+        }
+        private void Update()
+        {
+            if (environmentObjects.Length == 0 || idx >= environmentObjects.Length) return;
+            var targetPosition = environmentObjects[idx].pos.position;
+            Vector3 directionToTarget = (targetPosition - periscopeCamera.transform.position).normalized;
+            currentDot = Vector3.Dot(directionToTarget, periscopeCamera.transform.forward);
+            if (currentDot > threshHold)
+            {
+                lookTimer += Time.deltaTime;
+
+                if (lookTimer < requiredTime) return;
+                idx++;         
+                lookTimer = 0f;
+                // TODO: Play Bell SFX
+                if (idx < environmentObjects.Length)
+                    UpdateUI(environmentObjects[idx]);
+                else
+                    objectText.text = "All Targets Found!";
+            }
+            else
+            {
+                if (lookTimer >= 0) return;
+                lookTimer = 0f;
+            }
+        }
+        private void UpdateUI(EnvironmentObject target)
+        {
+            objectImage.sprite = target.objectImage;
+            StringBuilder sb =  new StringBuilder();
+            sb.Append($"Find: {target.name}!");
+            objectText.text = sb.ToString();
+            sb.Clear();
+        }
+    }
+}
